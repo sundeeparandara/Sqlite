e52ff401ef0a835182c972fedf703aeb46466b8f
SLA <40652679+sarandara@users.noreply.github.com>
2021-06-08 21:30:10 +0100

    update

--- RandomStore/main.py
+++ RandomStore/main.py
@@ -1,5 +1,6 @@
 import sqlite3
 import random
+import time
 from datetime import datetime
 
 
@@ -7,31 +8,50 @@ def create_db(name_of_db):
 
     connection = sqlite3.connect(name_of_db)
     cursor = connection.cursor()
+    # command1 = """CREATE TABLE IF NOT EXISTS
+    # random_table(ID INTEGER PRIMARY KEY, random_value REAL)"""
     command1 = """CREATE TABLE IF NOT EXISTS
-    random_table(ID INTEGER PRIMARY KEY, random_value REAL)"""
+    random_table(event_time INTEGER, random_value REAL)"""
     cursor.execute(command1)
 
-def update_db(name_of_db, event_time, data):
+def update_db(name_of_db, event_time, random_value):
     connection = sqlite3.connect(name_of_db)
     cursor = connection.cursor()
-    cursor.execute(f"INSERT INTO random_table VALUES ({event_time},{data})")
+    cursor.execute(f"INSERT INTO random_table VALUES ({event_time},{random_value})")
     connection.commit()
 
-rounds = 100
+rounds = 10
 i = 0
 name_of_db = 'random.db'
 
-#event_time = datetime.utcnow()
+
 
 while i < rounds:
 
+    event_time = int(time.time())
     value = random.uniform(0,1)
 
     if i == 0:
+        print("creating db")
+        print(event_time)
+        # a = datetime.now()
         create_db(name_of_db)
+        time.sleep(1)
+        # b = datetime.now()
+        # c = b - a
+        # print(f"{c.seconds} s")
+
     else:
-        update_db(name_of_db, i,value)
-        # print(event_time)
-        # print(type(event_time))
+        print(f"adding entry {i}")
+        print(event_time)
+        #a = datetime.now()
+        update_db(name_of_db, event_time,value)
+        time.sleep(1)
+        # b = datetime.now()
+        # c = b - a
+        # print(f"{c.seconds} s")
+
+
+
 
     i += 1
