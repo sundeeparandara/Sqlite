107a5c8cafb6ae259e42f9b86ef4531b608f0fa6
SLA <40652679+sarandara@users.noreply.github.com>
2021-06-08 21:35:54 +0100

    operational

--- RandomStore/main.py
+++ RandomStore/main.py
@@ -11,7 +11,7 @@ def create_db(name_of_db):
     # command1 = """CREATE TABLE IF NOT EXISTS
     # random_table(ID INTEGER PRIMARY KEY, random_value REAL)"""
     command1 = """CREATE TABLE IF NOT EXISTS
-    random_table(event_time INTEGER, random_value REAL)"""
+    random_table(event_time REAL, random_value REAL)"""
     cursor.execute(command1)
 
 def update_db(name_of_db, event_time, random_value):
@@ -28,7 +28,7 @@ name_of_db = 'random.db'
 
 while i < rounds:
 
-    event_time = int(time.time())
+    event_time = time.time()
     value = random.uniform(0,1)
 
     if i == 0:
@@ -36,7 +36,7 @@ while i < rounds:
         print(event_time)
         # a = datetime.now()
         create_db(name_of_db)
-        time.sleep(1)
+        #time.sleep(1)
         # b = datetime.now()
         # c = b - a
         # print(f"{c.seconds} s")
@@ -46,12 +46,9 @@ while i < rounds:
         print(event_time)
         #a = datetime.now()
         update_db(name_of_db, event_time,value)
-        time.sleep(1)
+        #time.sleep(1)
         # b = datetime.now()
         # c = b - a
         # print(f"{c.seconds} s")
-
-
-
 
     i += 1
