bfe444c1e003150ef45f98d52f56146a745b1898
SLA <40652679+sarandara@users.noreply.github.com>
2021-06-08 21:08:18 +0100

    update

--- RandomStore/main.py
+++ RandomStore/main.py
@@ -1,5 +1,6 @@
 import sqlite3
 import random
+from datetime import datetime
 
 
 def create_db(name_of_db):
@@ -7,19 +8,20 @@ def create_db(name_of_db):
     connection = sqlite3.connect(name_of_db)
     cursor = connection.cursor()
     command1 = """CREATE TABLE IF NOT EXISTS
-    random_table(random_value REAL)"""
+    random_table(ID INTEGER PRIMARY KEY, random_value REAL)"""
     cursor.execute(command1)
 
-def update_db(name_of_db, data):
+def update_db(name_of_db, event_time, data):
     connection = sqlite3.connect(name_of_db)
     cursor = connection.cursor()
-    cursor.execute(f"INSERT INTO random_table VALUES ({data})")
+    cursor.execute(f"INSERT INTO random_table VALUES ({event_time},{data})")
     connection.commit()
-
 
 rounds = 100
 i = 0
 name_of_db = 'random.db'
+
+#event_time = datetime.utcnow()
 
 while i < rounds:
 
@@ -28,6 +30,8 @@ while i < rounds:
     if i == 0:
         create_db(name_of_db)
     else:
-        update_db(name_of_db,value)
+        update_db(name_of_db, i,value)
+        # print(event_time)
+        # print(type(event_time))
 
     i += 1
